90ca372601c8df2974d8d58875a6d1e882d08830
Augustine Ogiza <austinogiza@gmail.com>
2024-02-24 22:08:27 +0100

    update site cache

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("690168cbcd6071f6")
+/******/ 		__webpack_require__.h = () => ("044126b887485ad1")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "81d02f6518651876"; }
+/******/ 		__webpack_require__.h = function() { return "0a7867787bd06372"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
