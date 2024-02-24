f7f212c286db8924e3e01c7702aaa35c31c3fd8b
Augustine Ogiza <austinogiza@gmail.com>
2024-02-24 21:58:00 +0100

    update site cache

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "95a8dbcf6017b4b6"; }
+/******/ 		__webpack_require__.h = function() { return "81d02f6518651876"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
